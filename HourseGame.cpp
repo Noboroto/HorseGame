38b57ae40036fdcb88eaf44e6ac07e0257caae6a
David0403 <48942146+Noboroto@users.noreply.github.com>
2021-11-14 00:43:59 +0700

    Update Player

--- HourseGame.cpp
+++ HourseGame.cpp
@@ -3,11 +3,49 @@
 
 using namespace std;
 
-struct Hourse {
+const int HOURSES_PER_PLAYER = 4;
 
+struct Hourse
+{
+	int PosX;
+	int PosY;
+	Hourse(int posx = 0, int posy = 0)
+	{
+		PosX = posx;
+		PosY = posy;
+	}
 };
 
-struct Point {
+struct Player
+{
+	string Username;
+	string Password;
+	string Name;
+	int Score;
+	
+	Hourse OwnHourse[HOURSES_PER_PLAYER];
+
+	Player(string username = "", string password = "", string name = "", int score = 0,
+			int startx = 0, int starty = 0)
+	{
+		Username = username;
+		Password = password;
+		Name = name;
+		Score = score;
+		for (int i = 0; i < HOURSES_PER_PLAYER; ++i)
+		{
+			OwnHourse[i] = Hourse(startx, starty);
+		}
+	}
+
+	bool isCorrect(string usename, string password)
+	{
+		return (usename == Username) && (password == Password);
+	}
+};
+
+struct Point 
+{
 
 };
 
